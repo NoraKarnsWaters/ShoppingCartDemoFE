1996862388a415e6cf252fb3c08001c85576afa6
Cedric Karns-Waters <nkarnswaters@gmail.com>
2022-07-14 18:08:01 -0400

    Wrapped up table field adding, default values and cart total

--- src/App.js
+++ src/App.js
@@ -2,44 +2,69 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
     static displayName = App.name;
+
+    getSum(cart) {
+        var sum = 0;
+        cart.forEach(x => { sum += x.price * x.qty });
+        return sum;
+    }
+
     createLine = () => {
-        var joined = this.state.cart;
+        var joined = [...this.state.cart, { id: this.state.cart.length + 1, name: "New Book", qty: 1, price: 10.99 }];
+        var total = this.getSum(joined);
+            
         this.setState(
             {
-                cart: [...joined, { id: joined.length + 1, name: "his", qty: 12, price: 104.99 }]
+                cart: joined,
+                total: total
             }            
         );
     }
 
-    constructor(props) {
-        super(props);
-        this.state = { cart: [{ id: 0, name: "hi", qty: 1, price: 10.99 }], loading: false };
+    handleChange(key, row, e) {
+        var newCart = this.state.cart;
+        newCart[row.id - 1][key] = e.target.value;
+        var total = this.getSum(newCart);
+
+        this.setState(
+            {
+                cart: newCart,
+                total: total
+            }
+        );
     }
 
-    
-    
-    
+    saveCart = () => {
 
-    static renderCartTable(cart) {
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { cart: [], total: 0, loading: false };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    static renderCartTable(cart, total, handleChange) {
         return (
             <table>
                 <thead>
                     <tr>
                         <th>Name</th>
+                        <th>Qty</th>
                         <th>Unit Price</th>
                         <th>Unit Price</th>
                         <th>Line Total</th>
-                        <th>Total</th>
+                        <th>Total: ${total}</th>                        
                     </tr>
                 </thead>
                 <tbody>
                     {cart.map(row => (
-                        <tr>
-                            <td><input value={row.name} /></td>
-                            <td><input value={row.qty} /></td>
-                            <td><input value={row.price} /></td>
-                            <td>$0</td>
-                            <td>$0</td>
+                        <tr key={row.id}>
+                            <td><input type="text" value={row.name} onChange={(e) => handleChange("name", row, e)}/></td>
+                            <td><input type="number" value={row.qty} onChange={(e) => handleChange("qty", row, e)} /></td>
+                            <td>$<input type="number" value={row.price} onChange={(e) => handleChange("price", row, e)}/></td>
+                            <td>${row.price}</td>
+                            <td>${row.price * row.qty}</td>
                         </tr>
                     ))}
                 </tbody>
@@ -76,12 +101,14 @@ export default class App extends Component {
 
 
     render() {
-        let contents = App.renderCartTable(this.state.cart);
+        let contents = App.renderCartTable(this.state.cart, this.state.total, this.handleChange);
 
         return (
             <div>
                 {contents}
-                <button onClick={this.createLine}>New</button>
+                <div>
+                    <button onClick={this.createLine}>New</button><button onClick={this.saveCart}>Save</button>
+                </div>
             </div>
         );
     }
